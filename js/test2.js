51a203a68e21386636a7e0a2278886a220cb66da
1144532284@qq.com <1144532284@qq.com>
2020-05-06 23:26:47 +0800

    1 init

--- js/test2.js
+++ js/test2.js
@@ -1,17 +1,2 @@
 //1 init
 console.log('1,init')
-
-//2 commit
-console.log('2 commit')
-
-//2 commit 2
-console.log('2 commit 2')
-
-//2 commit 3
-console.log('2 commit 3')
-
-//2 commit 4 
-console.log('2 commit 4')
-
-//1 commit 3
-console.log('1 commit 3')
