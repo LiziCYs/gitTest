7357a43194ae800eb9d52d551b225eaec3d90896
1144532284@qq.com <1144532284@qq.com>
2020-05-06 23:05:34 +0800

    2 commit 4

--- js/test2.js
+++ js/test2.js
@@ -8,4 +8,7 @@ console.log('2 commit')
 console.log('2 commit 2')
 
 //2 commit 3
-console.log('3 commit 3')
+console.log('2 commit 3')
+
+//2 commit 4 
+console.log('2 commit 4')
---
+++ js/test3.js
@@ -0,0 +1,2 @@
+//2 commit 4
+console.log('2 commit 4')
