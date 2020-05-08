75a29253f499869643bd5cc3920adebe89ee1080
1144532284@qq.com <1144532284@qq.com>
2020-05-08 11:00:42 +0800

    1 add

--- js/test1.js
+++ js/test1.js
@@ -1,13 +1,1 @@
-//1 init
-console.log('1,init')
-
-//1 commit 1
-console.log('1 commit')
-//2 commit
-console.log('2 commit')
-
-//1 commit 2 
-console.log('1 commit 2')
-
-//2 commit 2
-console.log('2 commit 2')
+console.log(1);
--- js/test3.js
+++
@@ -1,8 +0,0 @@
-//2 commit 4
-console.log('2 commit 4')
-
-//1 commit 5
-console.log('1 commit 5')
-
-//1 commit 6
-console.log('1 commit 6')
